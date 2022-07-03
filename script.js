7f705b7c8d603da503be507b2f0ea2932fd2893f
trupti0406 <truptivijayyadav27@gmail.com>
2022-07-03 21:14:24 +0530

    Till adding a new ticket

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let allColors = document.querySelectorAll(".color");
 let main = document.querySelector(".main");
+const addBtn = document.querySelector(".add");
+const colors = ["pink", "blue", "green", "black"];
+const colorBoxes = document.querySelectorAll(".color_boxes");
+const lock = document.querySelector(".lock");
+const unlock = document.querySelector(".unlock");
+const deleteBtn = document.querySelector(".delete");
+
+
+
 for (let i = 0; i < allColors.length; i++) {
   allColors[i].addEventListener("click", changeColor);
 }
@@ -13,4 +22,28 @@ function changeColor(e) {
   let color = allclasses[1];
   console.log(color);
   main.style.backgroundColor = color;
+}
+function createTicket() {
+  // 2. main -> ticket add
+  let id = uuidv4();
+  let ticket = document.createElement("div");
+  ticket.setAttribute("class", "ticket");
+  ticket.innerHTML = `
+    <div class="ticket_header black"></div>
+    <div class="ticket_content">
+        <div class="ticket_id ">
+            #${id}
+        </div>
+        <textarea name=""></textarea>
+    </div>
+`;
+  main.appendChild(ticket);
+  // ticket should have some functionalites
+
+  // 3. change color
+  // header pe click ho -> change the color
+  let header = ticket.querySelector(".ticket_header");
+  header.addEventListener("click", changeColor);
+  // 4. lock and unlock,color filtering
+  // diificult
 }
